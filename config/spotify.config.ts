e9b0a3d193a45a3b3e2d101ceaa1617cb5972d30
Jacky Wu <Jackyw1@uci.edu>
2021-06-29 13:22:10 -0700

    removed spotify API key

--- config/spotify.config.ts
+++ config/spotify.config.ts
@@ -1,6 +1,6 @@
 export default {
   client_id: '57a2efad88444313a05aa797afb7757c',
-  client_secret: 'c5249efd61434eb288c2e4ce59f28ac6',
+  client_secret: 'c9f8e9b050a0420e94a05020a85afc72',
   redirect_uri: 'http://127.0.0.1:1337/spotify/authorize',
   state: '420testtiddies69',
 };
