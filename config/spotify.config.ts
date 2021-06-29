980a4f631fa069c732e9a9953b9123f8521ead22
Jacky Wu <Jackyw1@uci.edu>
2021-06-29 13:25:06 -0700

    updated file structure

--- config/spotify.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  client_id: '57a2efad88444313a05aa797afb7757c',
-  client_secret: 'c9f8e9b050a0420e94a05020a85afc72',
-  redirect_uri: 'http://127.0.0.1:1337/spotify/authorize',
-  state: '420testtiddies69',
-};
