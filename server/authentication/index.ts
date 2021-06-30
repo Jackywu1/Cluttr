a751fe7f6ed1a45872a571384b05ba5702ae6b5d
Jacky Wu <Jackyw1@uci.edu>
2021-06-29 17:34:17 -0700

    implement redis cache to use promises

--- server/authentication/index.ts
+++ server/authentication/index.ts
@@ -1,5 +1,9 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import spotify from './spotify';
+import youtube from './youtube';
 
 export default {
   spotify,
+  youtube,
 };
