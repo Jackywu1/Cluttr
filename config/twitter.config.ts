3dd25755ecedfdfe5784a9c390baaa27dbb9d18a
Jacky Wu <Jackyw1@uci.edu>
2021-07-01 13:38:16 -0700

    reset cache for gitignore

--- config/twitter.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  bearer_token: 'AAAAAAAAAAAAAAAAAAAAAH8aPwEAAAAA3uYj%2FPddOJlTs6px5xQ6CeRvAXI%3DfMNbrXtOt4gy3pCmv8Al7G78UhDxo8nkW47OdmmFOPaomhoJ9k',
-  access_token: '3250339614-asZQ7qIxV61CTIZLFuaLw95QOuYSbX7arwn1vl4',
-  access_token_secret: 'OIUzOny62lYBz3w7HpUIHLHv0RQuzqWf7kFgtccdAHGxz',
-  consumer_key: 'LHNiRX4WoqbHM4bNIYlg7cv40',
-  consumer_secret: '52orJ4fyoXBErimqpbFFkAfFs64zdzo61LzDyl3FqBngeXDVoH',
-  // token_secret: 'SEHPy52eAjPRfBv1UpuYjdVuLntafSsVH3ypcGH5GSd51',
-  // bearer_token: 'AAAAAAAAAAAAAAAAAAAAAH8aPwEAAAAA0AHYMPURKFyab7uHkoWyJfW3L7c%3DLmPiGtNFu61IyMLCEPqlrK3pLXadb8QD2oE4rL32pVnvKBl9lp',
-};
--- config/youtube.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  api_key: 'AIzaSyA6sUP3FQiLiadvocxlr2YObISEqpERv8E',
-  client_id: '158482713651-5jb9rhdb78eoi74ln6v44k1msit39cfg.apps.googleusercontent.com',
-  client_secret: 'u_hb0mVkKwVTWyAV0_xo-ePV',
-  redirect_uri: 'http://127.0.0.1:1337/youtube/authorize',
-};
