d54a14e76609ef31205076df55d093725d0d967d
Jacky Wu <Jackyw1@uci.edu>
2021-07-01 22:37:19 -0700

    rearrange services files

--- services.ts
+++ services.ts
@@ -1,8 +1,8 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import spotifyService from '../spotify';
-import youtubeService from '../youtube';
-import twitterService from '../twitter';
+import spotifyService from './spotify';
+import youtubeService from './youtube';
+import twitterService from './twitter';
 
 export default [
   spotifyService(),
