bf0e64e2e7d8ae7b265d6c466af6b010b7127119
Jacky Wu <Jackyw1@uci.edu>
2021-07-04 11:47:11 -0700

    remove api keys

--- twitter/config/twitter.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  bearer_token: 'AAAAAAAAAAAAAAAAAAAAAH8aPwEAAAAAshZUJcQq9DCuf2Dg5zkQUQorceM%3DmcfagjLuJTAE4J7eULIUyltKlXFBTeXFpkB5oRNSrkHaZaS1tz',
-  access_token: '3250339614-Q6gw5Hc5pzmbPRxbfG8n3i1rkeSXMaY9Nd11v1I',
-  access_token_secret: '5tS1lbfxq4fb964NOVQAosJHzli9laOgb9iqFoldIof7n',
-  consumer_key: 'afZ4wUE0sfREKCAtxAtYa7SaY',
-  consumer_secret: 'KL0G1Nl5O7T8ASbhPmKUEHAkwLpwzVxmqHEykxjPvCSdBH4Key',
-};
