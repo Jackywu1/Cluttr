c92c1f8b57fe23c17a28a20e7862592013bd4032
Jacky Wu <Jackyw1@uci.edu>
2021-08-24 21:05:30 -0700

    cache data from twitter routes

--- twitter/routes/userProfile.ts
+++ twitter/routes/userProfile.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable camelcase */
 import { Request, Response } from 'express';
@@ -5,6 +6,7 @@ import axios from 'axios';
 import querystring from 'querystring';
 
 import twitter from '../config/twitter.config';
+import cache from '../cache';
 
 const {
   bearer_token,
@@ -13,20 +15,39 @@ const {
 const userProfile = async (req: Request, res: Response) => {
   try {
     const { user } = req.params;
-    const query = querystring.stringify({
-      'user.fields': 'profile_image_url,public_metrics,pinned_tweet_id,description',
-    });
+    const cachedData = await cache.get(`user:${user}`);
 
-    const response = await axios({
-      url: `https://api.twitter.com/2/users/by/username/${user}?${query}`,
-      headers: {
-        'User-Agent': 'v2TweetLookupJS',
-        Authorization: `Bearer ${bearer_token}`,
-      },
-    });
+    if (cachedData) {
+      console.log('from cache');
+      res.status(200).send(JSON.parse(cachedData));
+    } else {
+      // const response = await axios({
+      //   url: `https://api.twitter.com/2/users/by/username/${user}?${query}`,
+      //   headers: {
+      //     'User-Agent': 'v2TweetLookupJS',
+      //     Authorization: `Bearer ${bearer_token}`,
+      //   },
+      // });
 
-    const { data } = response.data;
-    res.status(200).send(data);
+      const query = querystring.stringify({
+        'user.fields': 'profile_image_url,public_metrics,pinned_tweet_id,description',
+      });
+
+      const response = await axios.get(
+        `https://api.twitter.com/2/users/by/username/${user}?${query}`,
+        {
+          headers: {
+            'User-Agent': 'v2TweetLookupJS',
+            Authorization: `Bearer ${bearer_token}`,
+          },
+        },
+      );
+
+      const { data } = response.data;
+      cache.add(`user:${user}`, 60, JSON.stringify(data));
+
+      res.status(200).send(data);
+    }
   } catch (err) {
     res.status(404).send(err);
   }
--- twitter/routes/userTweets.ts
+++ twitter/routes/userTweets.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable camelcase */
 import { Request, Response } from 'express';
@@ -5,6 +6,7 @@ import axios from 'axios';
 import querystring from 'querystring';
 
 import twitter from '../config/twitter.config';
+import cache from '../cache';
 
 const {
   bearer_token,
@@ -13,20 +15,30 @@ const {
 const userTweets = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const query = querystring.stringify({
-      user_id: id,
-    });
+    const cachedData = await cache.get(`id:${id}`);
 
-    const response = await axios({
-      url: `https://api.twitter.com/1.1/statuses/user_timeline.json?${query}`,
-      headers: {
-        'User-Agent': 'v2TweetLookupJS',
-        Authorization: `Bearer ${bearer_token}`,
-      },
-    });
+    if (cachedData) {
+      console.log('from cache');
+      res.status(200).send(JSON.parse(cachedData));
+    } else {
+      console.log('from api');
+      const query = querystring.stringify({
+        user_id: id,
+      });
 
-    const { data } = response;
-    res.status(200).send(data);
+      const response = await axios({
+        url: `https://api.twitter.com/1.1/statuses/user_timeline.json?${query}`,
+        headers: {
+          'User-Agent': 'v2TweetLookupJS',
+          Authorization: `Bearer ${bearer_token}`,
+        },
+      });
+
+      const { data } = response;
+      cache.add(`id:${id}`, 60, JSON.stringify(data));
+
+      res.status(200).send(data);
+    }
   } catch (err) {
     res.status(404).send(err);
   }
